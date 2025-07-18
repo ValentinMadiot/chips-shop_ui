4ceba70e7fae7af78e43c37eb4d1c2c9c13cf2d9
valentinmadiot <valentin.madiot@gmail.com>
2025-07-18 23:31:02 +0700

    add navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,36 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav_menu"),
+  navToggle = document.getElementById("nav_toggle"),
+  navClose = document.getElementById("nav_close");
 
+// Menu Show
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show_menu");
+  });
+}
+
+// Menu Hidden
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show_menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav_link");
 
+const linkAction = () => {
+  navMenu.classList.remove("show_menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SHADOW HEADER ===============*/
 
+/*=============== SWIPER FAVORITES ===============*/
 
-/*=============== SWIPER FAVORITES ===============*/ 
-
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
