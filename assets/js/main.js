163e4378740f65c9621f89ad28f4cd0e3d1dc48b
valentinmadiot <valentin.madiot@gmail.com>
2025-07-19 00:10:28 +0700

    add favorite section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,46 @@
 /*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById("nav_menu"),
-  navToggle = document.getElementById("nav_toggle"),
-  navClose = document.getElementById("nav_close");
+const navMenu = document.getElementById("nav__menu"),
+  navToggle = document.getElementById("nav__toggle"),
+  navClose = document.getElementById("nav__close");
 
 // Menu Show
 if (navToggle) {
   navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show_menu");
+    navMenu.classList.add("show__menu");
   });
 }
 
 // Menu Hidden
 if (navClose) {
   navClose.addEventListener("click", () => {
-    navMenu.classList.remove("show_menu");
+    navMenu.classList.remove("show__menu");
   });
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll(".nav_link");
+const navLink = document.querySelectorAll(".nav__link");
 
 const linkAction = () => {
-  navMenu.classList.remove("show_menu");
+  navMenu.classList.remove("show__menu");
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SHADOW HEADER ===============*/
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("shadow__header")
+    : header.classList.remove("shadow__header");
+};
+window.addEventListener("scroll", shadowHeader);
 
 /*=============== SWIPER FAVORITES ===============*/
+const swiperFavorites = new Swiper(".favorite__swiper", {
+  loop: true,
+  grabCursor: true,
+  slidesPerView: "auto",
+  centeredSlides: "auto",
+});
 
 /*=============== SHOW SCROLL UP ===============*/
 
